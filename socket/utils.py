716a04919dfef5966a075e057435a70c3e80f382
Patrick <huangpatrick16777216@gmail.com>
2020-10-10 22:41:13 -0700

    Function: Receive socket message.

---
+++ socket/utils.py
@@ -0,0 +1,31 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+# 
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+def Receive(conn, header=256):
+    """
+    Receives a message from a socket connection.
+    :param conn: Connection obtained from \"conn, addr = server.accept()\"
+    :param header: Amount of bytes in header message.
+    :return: class bytes, received message.
+    """
+
+    msg = conn.recv(header)
+    while not msg:
+        msg = conn.recv(header)
+
+    msg = conn.recv(int(msg))
+    return msg
