6d56c6fe8bcc018943f441083b24dd77f2145d19
Patrick <huangpatrick16777216@gmail.com>
2020-10-17 11:12:12 -0700

    Edit: Added definition for init.

--- pumpkinpy/pygameutils/elements.py
+++ pumpkinpy/pygameutils/elements.py
@@ -23,8 +23,20 @@ class ButtonText:
     button.Draw(WINDOW); button.Update() in game loop
     button.clicked to check if clicked.
     """
-    
     def __init__(self, loc, size, bgColIdle, bgColHover, bgColPress, text, textOffset, border=0, borderCol=None, clickButton=0):
+        """
+        Initializes the button.
+        :param loc: Tuple specifying pixel location: (100, 100)
+        :param size: Tuple specifying pixel width and height dimensions: (200, 50)
+        :param bgColIdle: Color (RGB) when not hovered over or clicked.
+        :param bgColHover: Color (RGB) when hovered but not clicked.
+        :param bgColPress: Color (RGB) when clicked.
+        :param text: Pygame text object (obtained from font.render())
+        :param textOffset: Offset location of text from center of button: (10, 10)
+        :param border: Width of border (set to 0 to disable).
+        :param borderCol=None: Color of border (ignored if no border).
+        :param clickButton: Mouse button to register as a click (0 for left, 1 for middle, and 2 for right).
+        """
         self.loc = loc
         self.size = size
         self.textLoc = (loc[0] + textOffset[0] + (size[0]-text.get_width())//2, loc[1] + textOffset[1] + (size[1]-text.get_height())//2)
