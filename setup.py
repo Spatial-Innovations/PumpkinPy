4548ccc1b2bd43ea6b2650b9756dfed02465ab7c
Patrick <huangpatrick16777216@gmail.com>
2020-10-26 19:36:50 -0700

    Edit: Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pumpkinpy",
-    version="0.2.0",
+    version="0.2.1",
     author="Spatial Innovations",
     author_email="spatialinnovations@gmail.com",
     description="A Python module with utilities for many fields.",
