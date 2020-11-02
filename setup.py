c6402369d9cfb23d45e8f97bff3fa05ffbf120e5
Arjun <iarjun.sahlot@gmail.com>
2020-11-01 16:10:18 -0800

    New Version: 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pumpkinpy",
-    version="0.3.2",
+    version="0.3.3",
     author="Spatial Innovations",
     author_email="spatialinnovations@gmail.com",
     description="A Python module with utilities for many fields.",
