092b29e7348f097765daa76afef73c171c3434b5
Patrick <huangpatrick16777216@gmail.com>
2020-11-05 09:24:18 -0800

    IntProp.

--- pumpkinpy/pygameutils/interface/props.py
+++ pumpkinpy/pygameutils/interface/props.py
@@ -17,3 +17,32 @@
 
 def BoolProp(name="", description="", default=False):
     return {"type": "bool", "name": name, "description": description, "default": default}
+
+
+def IntProp(name="", description="", default=0, **kwargs):
+    final = {"type": "int", "name": name,
+             "description": description, "default": default}
+
+    if "min" in kwargs:
+        minimum = kwargs["min"]
+        if minimum > default:
+            raise ValueError("Minimum is greater than default.")
+        final["min"] = minimum
+
+        if "soft_min" in kwargs:
+            soft_min = kwargs["soft_min"]
+            if soft_min < minimum:
+                raise ValueError("Soft minimum is less than minimum.")
+            final["soft_min"] = soft_min
+
+    if "max" in kwargs:
+        maximum = kwargs["max"]
+        if maximum < default:
+            raise ValueError("Maximum is smaller than default.")
+        final["max"] = maximum
+
+        if "soft_max" in kwargs:
+            soft_max = kwargs["soft_max"]
+            if soft_max > maximum:
+                raise ValueError("Soft maximum is greater than maximum.")
+            final["soft_max"] = soft_max
