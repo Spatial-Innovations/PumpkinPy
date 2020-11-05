ad56f724d12ccc1c153d53e0f9847e4b99cb45dc
Patrick <huangpatrick16777216@gmail.com>
2020-11-05 09:25:43 -0800

    Added FloatProp.

--- pumpkinpy/pygameutils/interface/props.py
+++ pumpkinpy/pygameutils/interface/props.py
@@ -48,3 +48,34 @@ def IntProp(name="", description="", default: int = 0, **kwargs):
             final["soft_max"] = soft_max
 
     return final
+
+
+def FloatProp(name="", description="", default: int = 0, **kwargs):
+    final = {"type": "float", "name": name,
+             "description": description, "default": default}
+
+    if "min" in kwargs:
+        minimum = kwargs["min"]
+        if minimum > default:
+            raise ValueError("Minimum is greater than default.")
+        final["min"] = minimum
+
+        if "soft_min" in kwargs:
+            soft_min = kwargs["soft_min"]
+            if soft_min < minimum:
+                raise ValueError("Soft minimum is less than minimum.")
+            final["soft_min"] = soft_min
+
+    if "max" in kwargs:
+        maximum = kwargs["max"]
+        if maximum < default:
+            raise ValueError("Maximum is smaller than default.")
+        final["max"] = maximum
+
+        if "soft_max" in kwargs:
+            soft_max = kwargs["soft_max"]
+            if soft_max > maximum:
+                raise ValueError("Soft maximum is greater than maximum.")
+            final["soft_max"] = soft_max
+
+    return final
