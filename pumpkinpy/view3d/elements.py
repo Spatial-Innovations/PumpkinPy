0ae9ebaf101c8bfaa2116413866664231cce7562
Patrick <huangpatrick16777216@gmail.com>
2020-11-26 19:11:02 -0800

    Triangulate and export.

--- pumpkinpy/view3d/elements.py
+++ pumpkinpy/view3d/elements.py
@@ -15,6 +15,9 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import json
+import pickle
+
 
 class Mesh:
     def __init__(self, verts, faces, colors):
@@ -43,5 +46,50 @@ class Mesh:
         self.faces = []
         for i, face in enumerate(faces):
             self.faces.append([colors[i], []])
-            for i in face:
-                self.faces[-1][1].append(verts[i])
+            for j in face:
+                self.faces[-1][1].append(verts[j])
+
+    def modifier_triangluate(self):
+        """
+        Modifier: Triangulates mesh (divides all n-gons to triangles).
+        Note: This is required before rendering.
+        """
+        new_faces = []
+        for face in self.faces:
+            num_verts = len(face[1])
+            if num_verts == 3:
+                new_faces.append(face)
+            else:
+                color = face[0]
+                for i in range(1, num_verts-1):
+                    new_faces.append((color, (face[1][0], face[1][i], face[1][i+1])))
+
+        self.faces = new_faces
+
+    def export_json(self, path):
+        """
+        Exports data as a json
+        :param path: Path to export. The file will be overwritten, if it exists.
+        :type path: str
+        """
+        info = {"verts": self.verts, "faces": [], "colors": []}
+        for face in self.faces:
+            info["colors"].append(face[0])
+            info["faces"].append(face[1])
+
+        with open(path, "w") as file:
+            json.dump(info, file)
+
+    def export_binary(self, path):
+        """
+        Exports data as a binary file.
+        :param path: Path to export. The file will be overwritten, if it exists.
+        :type path: str
+        """
+        info = {"verts": self.verts, "faces": [], "colors": []}
+        for face in self.faces:
+            info["colors"].append(face[0])
+            info["faces"].append(face[1])
+
+        with open(path, "wb") as file:
+            pickle.dump(info, file)
